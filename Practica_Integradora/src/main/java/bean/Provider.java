802d1b0f153675708c1c62e150119c596b112b41
Elgussgood <gsuarezr2000@alumno.ipn.mx>
2022-09-30 19:22:33 -0500

    AES2

--- Practica_Integradora/src/main/java/bean/Provider.java
+++
@@ -1,9 +0,0 @@
-package bean;
-
-public interface Provider {
-String DRIVER="com.microsoft.sqlserver.jdbc.SQLServerDriver";
-String CONNECTION_URL="jdbc:sqlserver://148.204.168.150:1433;databaseName=db_LPTI_II";
-String USERNAME="sa";
-String PASSWORD="n0m3l0s3";
-
-}
